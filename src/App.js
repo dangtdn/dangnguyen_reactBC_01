50e1975ad6e80cdb164abe7c7d68f0c2228f7819
dangtdn <nguyendanglop93@gmail.com>
2021-02-02 20:01:56 +0700

    updatedBuoi1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import BaiTapLayout from './components/BaiTapLayout/BaiTapLayout';
 import DataBinding from './DataBinding.js/DataBinding';
+import HandleEvent from './HandleEvent/HandleEvent';
+import DemoState from './StateReact/DemoState';
+import BaiTapChonXe from './StateReact/BaiTapChonXe';
 
 function App() {
   return (
     <div className="App">
       {/* <BaiTapLayout/> */}
-      <DataBinding/>
+      {/* <DataBinding/> */}
+      {/* <HandleEvent/> */}
+      {/* <DemoState/> */}
+      <BaiTapChonXe/>
     </div>
   );
 }
